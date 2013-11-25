13863c3849bb1813ddfe2311391933855a1489be
gulian <gulian@orange.fr>
2013-11-25 01:26:00 +0100

    add some beauty and interaction

--- public/javascripts/controllers/accountDetailsController.js
+++ public/javascripts/controllers/accountDetailsController.js
@@ -2,16 +2,26 @@ angular.module('drunkennemesis').controller('accountDetailsController', function
 
     $scope.account = Account.get({
         accountId : $routeParams.account
+    }, function(){
+        $scope.processBalance();
     });
+
+    $scope.processBalance =  function(){
+        $scope.balance = 0 ;
+        for(var i in $scope.account.operations){
+            $scope.balance += $scope.account.operations[i].amount;
+        }
+    };
 
     $scope.newOperation = function(){
         var debug = {
             name : "Operation test",
-            amount: Math.random()*10 - 5 ,
+            amount: Math.random()*100 - 50 ,
             _account : $scope.account._id
         };
         Operation.post(debug,function(operation){
             $scope.account.operations.push(operation);
+            $scope.processBalance();
         });
     };
 
