547e8f587813df72996085137921c027a9fc3cea
gulian <gulian@gulian.fr>
2013-11-25 13:42:58 +0100

    It's now possible to perform batch action

--- public/javascripts/controllers/accountDetailsController.js
+++ public/javascripts/controllers/accountDetailsController.js
@@ -1,5 +1,7 @@
 angular.module('drunkennemesis').controller('accountDetailsController', function ($scope, $routeParams, Account, Operation){
 
+    $scope.selectedOperationsCount = 0;
+
     $scope.account = Account.get({
         accountId : $routeParams.account
     }, function(){
@@ -36,10 +38,54 @@ angular.module('drunkennemesis').controller('accountDetailsController', function
     };
 
     $scope.update = function (operation) {
-        // var operation = $scope.account.operations[$index];
         Operation.put(operation);
         $scope.processBalance();
         $scope.processForecastBalance();
-    }
+    };
+
+    $scope.updateSelected = function(all, newState){
+        if(all){
+            for(var i in $scope.account.operations){
+                $scope.account.operations[i].selected = $scope.allSelected;
+            }
+            $scope.selectedOperationsCount = $scope.allSelected ? $scope.account.operations.length : 0 ;
+        } else {
+            $scope.selectedOperationsCount += newState ? 1 : -1 ;
+            if($scope.selectedOperationsCount === $scope.account.operations.length){
+                $scope.allSelected = true ;
+            } else {
+                $scope.allSelected = false ;
+            }
+        }
+
+    };
+
+    $scope.checkSelected = function(){
+        for(var i in $scope.account.operations){
+            if($scope.account.operations[i].selected && !$scope.account.operations[i].checked){
+                $scope.account.operations[i].checked = true;
+                Operation.put($scope.account.operations[i]);
+            }
+        }
+        $scope.processBalance();
+        $scope.processForecastBalance();
+    };
+
+    $scope.uncheckSelected = function(){
+        for(var i in $scope.account.operations){
+            if($scope.account.operations[i].selected && $scope.account.operations[i].checked){
+                $scope.account.operations[i].checked = false;
+                Operation.put($scope.account.operations[i]);
+            }
+        }
+        $scope.processBalance();
+        $scope.processForecastBalance();
+    };
+
+    $scope.deleteSelected = function(){
+
+    };
+
+
 
 });
