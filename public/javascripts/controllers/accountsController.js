6265329aa3c1858b1ceef78292996d4ef79b9cd6
gulian <gulian@orange.fr>
2013-11-29 23:40:09 +0100

    you can now go to your brand new account just after clicked it

--- public/javascripts/controllers/accountsController.js
+++ public/javascripts/controllers/accountsController.js
@@ -3,9 +3,10 @@ angular.module('drunkennemesis').controller('accountsController', function ($sco
     $scope.accounts = Account.query() ;
 
     $scope.newAccount = function(){
-        var debug = {name:'Compte courant', bank:'SG', initialBalance: Math.floor(Math.random()*100)};
-        Account.post(debug);
-        $scope.accounts.push(debug);
+        var _account = {name:'Compte courant', bank:'SG', initialBalance: Math.floor(Math.random()*100)};
+        Account.post(_account, function(account){
+            $scope.accounts.push(account);
+        });
     };
 
 });
